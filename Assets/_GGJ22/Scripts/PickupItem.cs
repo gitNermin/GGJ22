85ad7b60f08a5d8c09b3b9ed112aa7dc82708ab7
Nermin <shawkat.nermin@gmail.com>
2022-01-29 13:51:59 +0200

    Pickup and Drop

--- Assets/_GGJ22/Scripts/PickupItem.cs
+++ Assets/_GGJ22/Scripts/PickupItem.cs
@@ -1,11 +1,53 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupItem : Interactable
 {
-    public override void DoAction()
+    public static PickupItem CurrentPickupItem;
+    
+    [SerializeField] private Sprite _thumbnail;
+    [SerializeField] private string _name;
+
+    public Sprite Thumbnail => _thumbnail;
+    public string Name => _name;
+
+    private Collider _collider;
+    private Rigidbody _rigidBody;
+
+    private void Start()
     {
-        Debug.Log("Hello");
+        _collider = GetComponent<Collider>();
+        _rigidBody = gameObject.AddComponent<Rigidbody>();
+    }
+
+    public override void DoAction(GameObject player)
+    {
+        PickupItemsController _controller = player.GetComponent<PickupItemsController>();
+        
+        if (_controller)
+        {
+            _controller.Pickup(this);
+            _collider.enabled = false;
+            Destroy(_rigidBody);
+        }
+    }
+
+    public void Throw(Vector2 force)
+    {
+        _rigidBody = gameObject.AddComponent<Rigidbody>();
+        _collider.enabled = true;
+        _rigidBody.AddForce(force);
+    }
+
+    public void AddToInventory()
+    {
+        
+    }
+
+    public void Pickup()
+    {
+        
     }
 }
