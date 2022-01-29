c8142dd10da18f7f85de34b26d0c38a6cf04361a
leckchess <huda.rasmey@gmail.com>
2022-01-29 04:27:46 +0200

    portal
    portal code and effect
    there is an error in the shader but it is working fine so please dont remove its line XD - it ll affect the shader

---
+++ Assets/_GGJ22/Scripts/Portal.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _portalB;
+    [SerializeField]
+    private float _portalsGapTime = 2;
+
+
+    private GameObject _myEffect;
+
+    private Transform _portalBPoint;
+    private GameObject _portalBEffect;
+
+    private GameObject _Item;
+
+    private void Start()
+    {
+        _myEffect = transform.GetChild(0).gameObject;
+        _portalBEffect = _portalB.transform.GetChild(0).gameObject;
+        _portalBPoint = _portalB.transform.GetChild(1); 
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.collider.CompareTag("Item"))
+        {
+            _myEffect.SetActive(true);
+            _Item = collision.gameObject;
+            StartCoroutine(TransferItem());
+        }
+    }
+
+    private IEnumerator TransferItem()
+    {
+        yield return new WaitForSeconds(_portalsGapTime);
+        _myEffect.SetActive(false);
+        _portalBEffect.SetActive(true);
+        _Item.SetActive(false);
+
+        yield return new WaitForSeconds(_portalsGapTime);
+        _portalBEffect.SetActive(false);
+        _Item.transform.position = _portalBPoint.position;
+        _Item.tag = "Untagged";
+        _Item.SetActive(true);
+    }
+}
