d4f1d12539286784ccec8c89b59897545f6f229a
leckchess <huda.rasmey@gmail.com>
2022-01-29 11:34:10 +0200

    radio tv puzzle

--- Assets/_GGJ22/Scripts/Radio.cs
+++ Assets/_GGJ22/Scripts/Radio.cs
@@ -7,6 +7,12 @@ public class Radio : MonoBehaviour
     [SerializeField]
     private AudioClip[] _channels;
 
+    [SerializeField]
+    private int _rightChannel;
+
+    [SerializeField]
+    private TV _tv;
+
 
     private AudioSource _audioSource;
     private int _currChannel = 0;
@@ -36,5 +42,20 @@ public class Radio : MonoBehaviour
         _audioSource.clip = _channels[_currChannel];
 
         _audioSource.Play();
+
+        if (_currChannel == _rightChannel && _tv && !_tv.canBeOpen)
+        {
+            _tv.canBeOpen = true;
+            _tv.SwitchONOFF();
+        }
     }
+
+    /*private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+            SwitchONOFF();
+
+        if (Input.GetMouseButtonDown(0))
+            ChangeChanel();
+    }*/
 }
--- Assets/_GGJ22/Scripts/TV.cs
+++ Assets/_GGJ22/Scripts/TV.cs
@@ -7,12 +7,39 @@ public class TV : MonoBehaviour
     [SerializeField]
     private GameObject[] _channels;
 
+    [SerializeField]
+    private GameObject _nosignalChannel;
+
+    public bool canBeOpen;
+
     private int _currChannel = 0;
+
+
+    public void SwitchONOFF()
+    {
+        if (!canBeOpen)
+            return;
+
+        _nosignalChannel.SetActive(!_nosignalChannel.activeSelf);
+        _channels[_currChannel].SetActive(!_nosignalChannel.activeSelf);
+    }
 
     public void ChangeChanel()
     {
+        if (_nosignalChannel.activeSelf)
+            return;
+
         _channels[_currChannel].SetActive(false);
         _currChannel = (_currChannel + 1) % _channels.Length;
         _channels[_currChannel].SetActive(true);
     }
+
+    /*private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+            SwitchONOFF();
+
+        if (Input.GetMouseButtonDown(0))
+            ChangeChanel();
+    }*/
 }
